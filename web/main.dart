5d090e80f4c9868b2587ff76583e781874554984
cosmicSigma <abdurashidxodiyev17@gmail.com>
2023-04-06 10:35:19 +0500

    test netlify

---
+++ web/main.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+void main() {
+  print(window.location.pathname);
+  if(window.location.pathname != '/index.html') {
+    print("Animation worked");
+  } else {
+    final a = querySelector('.link') as AnchorElement;
+    a.onClick.listen((event) {
+      window.location.href = "/registration/register.html";
+    });
+  }
+}
